02123b1fc53b715c8a4ba697ca8198e41d647283
Aniket Pant <63965053+aniketpant83@users.noreply.github.com>
2023-03-12 22:10:04 -0400

    Update lab2-Fat-Tree.py

--- lab2-Fat-Tree.py
+++ lab2-Fat-Tree.py
@@ -10,29 +10,29 @@ class FatTree(Topo):
         h = n // 2
 
         # Create core switches
-        core_switches = []
+        coreSwitches = []
         for i in range(n // 2):
-            core_switches.append(self.addSwitch('c{}'.format(i+1)))
+            coreSwitches.append(self.addSwitch('c{}'.format(i+1)))
 
         # Create aggregation switches
-        agg_switches = []
+        aggSwitches = []
         for i in range(n):
-            agg_switches.append(self.addSwitch('a{}'.format(i+1)))
+            aggSwitches.append(self.addSwitch('a{}'.format(i+1)))
 
         # Create edge switches and hosts
         for i in range(n):
-            edge_switch = self.addSwitch('e{}'.format(i+1))
+            edgeSwitch = self.addSwitch('e{}'.format(i+1))
             for j in range(h):
                 host = self.addHost('h{}{}'.format(i+1, j+1))
-                self.addLink(host, edge_switch, bw=10)
-            self.addLink(edge_switch, agg_switches[i // 2], bw=10)
+                self.addLink(host, edgeSwitch, bw=10)
+            self.addLink(edgeSwitch, aggSwitches[i // 2], bw=10)
 
         # Connect aggregation switches to core switches
         for i in range(n):
-            self.addLink(agg_switches[i], core_switches[i // (n // 2)], bw=20)
+            self.addLink(aggSwitches[i], coreSwitches[i // (n // 2)], bw=20)
 
 if __name__ == '__main__':
-    n = 4  # Change this to the desired value of n
+    n = 4  # Change as desired
     topo = FatTree(n)
     net = Mininet(topo=topo, link=TCLink)
     net.start()
